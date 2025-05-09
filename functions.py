42164a719b6910aa2ba36aa08b6186afa6632efc
elxxp <sch.jibreelbenjamin@gmail.com>
2025-05-09 19:16:17 +0700

    first commit

---
+++ functions.py
@@ -0,0 +1,75 @@
+def daftarBarang():
+    print("==================== INDOMARET ====================")
+    print("- semakin banyak belanja, semakin besar diskonnya - \n")
+    print(" DAFTAR BARANG")
+    print(" 1. Susu Rp 12,000      |   5. Minyak Rp 23,000")
+    print(" 2. Gula Rp 8,000       |   6. Sabun Rp 3,000")
+    print(" 3. Tepung Rp 20,000    |   7. Air putih Rp 2,000")
+    print(" 4. Beras Rp 50,000     |   8. Telur Rp 17,000")
+
+def getHarga(kodeProduk):
+    match kodeProduk:
+        case 1:
+            return 12000
+        case 2:
+            return 8000
+        case 3:
+            return 20000
+        case 4:
+            return 50000
+        case 5:
+            return 23000
+        case 6:
+            return 3000
+        case 7:
+            return 2000
+        case 8:
+            return 17000
+
+def getProduk(kodeProduk):
+    match kodeProduk:
+        case 1:
+            return "Susu"
+        case 2:
+            return "Gula"
+        case 3:
+            return "Tepung"
+        case 4:
+            return "Beras"
+        case 5:
+            return "Minyak"
+        case 6:
+            return "Sabun"
+        case 7:
+            return "Air putih"
+        case 8:
+            return "Telur"
+
+
+def totalPerProduk(produk, jumlah):
+    match produk:
+        case 1:
+            hasil = 12000 * jumlah
+            return hasil
+        case 2:
+            hasil = 8000 * jumlah
+            return hasil
+        case 3:
+            hasil = 20000 * jumlah
+            return hasil
+        case 4:
+            hasil = 50000 * jumlah
+            return hasil
+        case 5:
+            hasil = 23000 * jumlah
+            return hasil
+        case 6:
+            hasil = 3000 * jumlah
+            return hasil
+        case 7:
+            hasil = 2000 * jumlah
+            return hasil
+        case 8:
+            hasil = 17000 * jumlah
+            return hasil
+            
---
+++ penjualan.py
@@ -0,0 +1,64 @@
+import functions
+
+selesai = False
+subtotal = 0
+pesanan = []
+
+functions.daftarBarang()
+while selesai == False:
+    produk = int(input("\n = silahkan pilih produk yang ingin dibeli: "))
+
+    if produk > 0 and produk < 9:
+
+        print(f" = memilih produk {functions.getProduk(produk)} Rp {functions.getHarga(produk):,}")
+        
+        jumlah = int(input(" = silahkan masukan jumlah produk: "))
+
+        pesanan += [[produk, jumlah]]
+        subtotal += functions.totalPerProduk(produk, jumlah)
+
+        status = input("\n = ingin memesan produk lagi? (y/n): ")
+        if status == "y" or status == "Y":
+            continue
+        elif status == "n" or status == "N":
+            selesai = True
+        else:
+            print(" = error, kondisi tidak tersedia. \n")
+            break
+
+    else: #if cek produk
+        print(" = error, produk tidak ditemukan \n")
+        
+
+else: #while
+    if subtotal >= 100000 and subtotal < 200000:
+        diskon = "10%"
+        total = subtotal - (10/100 * subtotal)
+        potongan = subtotal - total
+    elif subtotal >= 200000 and subtotal <= 300000:
+        diskon = "20%"
+        total = subtotal - (20/100 * subtotal)
+        potongan = subtotal - total
+    elif subtotal >= 300000:
+        diskon = "35%"
+        total = subtotal - (35/100 * subtotal)
+        potongan = subtotal - total
+    else: 
+        diskon = "-"
+        total = subtotal
+        potongan = 0
+    
+    print("\n\n================= PESANAN SELESAI =================\n")
+    nomer = -1
+    for dataPesanan in pesanan:
+        order = nomer + 1
+
+        totalPerProduk = functions.totalPerProduk([dataPesanan][order][0], [dataPesanan][order][1])
+        print(f" = Rp {totalPerProduk:,} ... {[dataPesanan][order][1]}x {functions.getProduk([dataPesanan][order][0])}")
+
+    print(f"\n = Subtotal : Rp {subtotal:,}")
+    print(f" = Diskon : {diskon}")
+    print(f" = Potongan harga : -Rp {potongan:,}")
+    print(f" = Total : Rp {total:,}")
+    print("\n========== TERIMA KASIH TELAH BERBELANJA ==========\n")
+    
